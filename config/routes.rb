91004b4ad1ccf00194af6a332d8c11bbfeacbdbb
hikari24 <a.hikari24@gmail.com>
2020-04-05 10:23:44 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
 
   root 'home#top'
+  get 'home/about'
 
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
-  
-  
-  get 'home/about'
+
+  resources :users do
+    member do
+     get :following, :followers
+    end
+  end
+  resources :relationships,       only: [:create, :destroy]
+
 end
