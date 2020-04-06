6a0d201a24bfd7271344392c071ba7cbf4022388
hikari24 <a.hikari24@gmail.com>
2020-04-06 04:49:05 +0000

    finish

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,13 @@
 class RelationshipsController < ApplicationController
 	def create
-    @user = User.find(params[:relationship][:following_id])
-    current_user.follow!(@user)
-    redirect_to users_path
+    user = User.find(params[:relationship][:following_id])
+    current_user.follow!(user)
+    redirect_to request.referrer || root_url
   end
 
   def destroy
-    @user = Relationship.find(params[:id]).following
-    current_user.unfollow!(@user)
-    redirect_to users_path
+    user = Relationship.find(params[:id]).following
+    current_user.unfollow!(user)
+    redirect_to request.referrer || root_url
   end
 end
